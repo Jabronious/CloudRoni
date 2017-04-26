e66824b63b7a8b35ad3452bf803be5ef7da0ac88
Jarrod Merryman <jarrodmerryman@gmail.com>
2017-04-25 21:46:40 -0700

    added somes views and stuff

--- CloudRoni/urls.py
+++ CloudRoni/urls.py
@@ -4,4 +4,8 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    # ex: CloudRoni/5/
+    url(r'^(?P<team_id>[0-9]+)/$', views.team, name='team'),
+     # ex: CloudRoni/34/players/2/
+    url(r'^(?P<team_id>[0-9]+)/players/(?P<player_id>[0-9]+)/$', views.players, name='players'),
 ]
--- CloudRoni/views.py
+++ CloudRoni/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+
+from .models import Team, UserPlayer
  
 def index(request):
- return HttpResponse("Index")
+	teams_list = Team.objects.order_by('-created_date')[:10]
+	template = loader.get_template('teams/index.html')
+	context = {
+		'teams_list': teams_list,
+	}
+	return HttpResponse(template.render(context, request))
+
+def team(request, team_id):
+	response = "Here is a team %s!"
+	return HttpResponse(response % team_id)
+
+def players(request, team_id, player_id):
+	return HttpResponse("Here is a player {} from team # {}".format(player_id, team_id))
