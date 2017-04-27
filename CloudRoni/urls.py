d6004e5c6e5bd6b94885f0e63b4cb2ee0af92c7c
Jarrod Merryman <jarrodmerryman@gmail.com>
2017-04-26 21:54:14 -0700

    adds new views for teams and players

--- CloudRoni/urls.py
+++ CloudRoni/urls.py
@@ -2,10 +2,12 @@ from django.conf.urls import url
 
 from . import views
 
+app_name = 'cloud_roni'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    # ex: CloudRoni/5/
-    url(r'^(?P<team_id>[0-9]+)/$', views.team, name='team'),
+    url(r'^(?P<team_id>[0-9]+)/$', views.detail, name='detail'),
+    # ex: CloudRoni/5/team
+    url(r'^(?P<team_id>[0-9]+)/team/$', views.team, name='team'),
      # ex: CloudRoni/34/players/2/
     url(r'^(?P<team_id>[0-9]+)/players/(?P<player_id>[0-9]+)/$', views.players, name='players'),
 ]
--- CloudRoni/views.py
+++ CloudRoni/views.py
@@ -1,20 +1,32 @@
-from django.shortcuts import render
+from django.http import Http404
+from django.shortcuts import get_object_or_404, render
+
 from django.http import HttpResponse
 from django.template import loader
 
 from .models import Team, UserPlayer
- 
+
 def index(request):
-	teams_list = Team.objects.order_by('-created_date')[:10]
-	template = loader.get_template('teams/index.html')
-	context = {
+    teams_list = Team.objects.order_by('-created_date')[:10]
+    context = {
 		'teams_list': teams_list,
 	}
-	return HttpResponse(template.render(context, request))
+    return render(request, 'teams/index.html', context)
 
 def team(request, team_id):
+	team = get_object_or_404(Team, pk=team_id)
+	return render(request, 'teams/detail.html', {'team': team}) 
+
+def detail(request, team_id):
 	response = "Here is a team %s!"
 	return HttpResponse(response % team_id)
 
 def players(request, team_id, player_id):
-	return HttpResponse("Here is a player {} from team # {}".format(player_id, team_id))
+	player = get_object_or_404(UserPlayer, pk=player_id)
+	team = get_object_or_404(Team, pk=team_id)
+	context = {
+		'player': player,
+		'team': team
+	}
+	return render(request, 'players/index.html', context)
+	#return HttpResponse("Here is a player {} from team # {}".format(player_id, team_id))
