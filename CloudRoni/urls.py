cc350d5b22c087d7dd14e7456b1a0b2bc407a258
Jarrod Merryman <jarrodmerryman@gmail.com>
2017-05-02 20:52:47 -0700

    removes unnecessary url

--- CloudRoni/urls.py
+++ CloudRoni/urls.py
@@ -5,7 +5,6 @@ from . import views
 app_name = 'cloud_roni'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^(?P<team_id>[0-9]+)/$', views.detail, name='detail'),
     # ex: CloudRoni/5/team
     url(r'^(?P<team_id>[0-9]+)/team/$', views.team, name='team'),
      # ex: CloudRoni/34/players/2/
