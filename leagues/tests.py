ac8f6bae741d85a5df2746d60427821acad5e896
Jarrod <jarrodmerryman@gmail.com>
2018-02-08 17:53:30 +0000

    adds test for home page

--- leagues/tests.py
+++ leagues/tests.py
@@ -2,10 +2,14 @@
 from __future__ import unicode_literals
 
 from django.test import TestCase
+from leagues.models import League
 
 # Create your tests here.
 class LeagueViewTests(TestCase):
-    def test_teams_page_has_correct_info_with_no_teams(self):
+    def setUp(self):
+        self.home_page_request = self.client.get('/')
+
+    def team_home_page_request(self):
         content = self.home_page_request.content
 
         self.assertIn('Welcome to', content)
